af8fdb0e967737316b885ff6a9937e94000673c0
Santosh kumar <santosh.frev@gmail.com>
2020-04-14 15:13:55 +0530

    Assignment 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { MatGridListModule } from '@angular/material/grid-List';
 import { MatCardModule } from '@angular/material/card';
 import {MatButtonModule } from '@angular/material/button';
 import 'hammerjs';
-import { MenuComponent } from './menu/menu.component' ;
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component' ;
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
